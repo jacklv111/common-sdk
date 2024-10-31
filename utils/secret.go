c9c50d0fd9b1f43664c3b13be4cd9fa56f67f57c
jacklv111 <lv523954325@163.com>
2024-11-01 01:11:08 +0800

    add genaksk

--- utils/secret.go
+++ utils/secret.go
@@ -10,6 +10,7 @@ package utils
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/hex"
 )
 
@@ -20,4 +21,27 @@ func GenerateSecret() (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(key), nil
+}
+
+// generateKey 生成指定长度的随机字符串
+func generateKey(length int) (string, error) {
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(bytes)[:length], nil
+}
+
+// generateAKSK 生成 Access Key 和 Secret Key
+func GenerateAKSK() (string, string, error) {
+	ak, err := generateKey(16) // 生成 16 字节的 Access Key
+	if err != nil {
+		return "", "", err
+	}
+	sk, err := generateKey(32) // 生成 32 字节的 Secret Key
+	if err != nil {
+		return "", "", err
+	}
+	return ak, sk, nil
 }
